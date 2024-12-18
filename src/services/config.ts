16221280cf3c1b4caf9284a364587730f94ae233
DroppedHard <szyper7@gmail.com>
2024-12-18 23:43:47 +0100

    Update readme + fix env problem

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,2 +1,3 @@
 export const ALERT_AUTO_HIDE_DURATION = 4000;
-export const API = "http://localhost:8081/api/v1/";
+export const API =
+	import.meta.env.REACT_APP_API_URL || "http://localhost:8081/api/v1/";
